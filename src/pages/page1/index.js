a05f43cc03397b1f1ebc245bad891860e157c0d6
huzeqiang <1559173434@qq.com>
2018-11-08 18:41:24 +0800

    zhuanpan

--- src/pages/page1/index.js
+++ src/pages/page1/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import './index.css'
+import img1 from '../../imgs/1.png'
+import img3 from '../../imgs/3.png'
 export default class Page1 extends React.Component{
     constructor(){
         super()
@@ -11,6 +13,11 @@ export default class Page1 extends React.Component{
         return(
             <div>
                 Page1
+                <div className='bgdiv'>
+                    <img className="img1" src={img1} alt="pointer"/>
+                    <img className="img3" src={img3} alt="turntable"/>
+
+                </div>
             </div>
         )
     }
